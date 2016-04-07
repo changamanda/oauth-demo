5a0e96a899934606556153dc9b47c90cccb551f0
changamanda <amanda.chang10@gmail.com>
2016-04-07 12:23:47 -0500

    Working

--- routes/index.js
+++ routes/index.js
@@ -2,43 +2,33 @@ var express = require('express');
 var router = express.Router();
 
 var request = require('request');
-var CryptoJS = require("crypto-js");
+var querystring = require('querystring');
 
-/* GET home page. */
 router.get('/', function(req, res) {
-  var key = process.env.TWITTER_CONSUMER_KEY;
-  var nonce = "kYjzVBB8Y0ZFabxSWbWovY3uYSQ2pTgmZeNu2VS4cg";
-  var method = "HMAC-SHA1";
-  var seconds = parseInt(new Date()/1000);
-  var access_token = process.env.TWITTER_ACCESS_TOKEN;
-  var version = "1.0";
-
-  var params = `oauth_consumer_key=${escape(key)}&oauth_nonce=${escape(nonce)}&oauth_signature_method=${method}&oauth_timestamp=${seconds}&oauth_token=${escape(access_token)}&oauth_version=${version}`;
-
-  var base = `POST&${escape('https://api.twitter.com/oauth/request_token')}&${params}`;
-  var signingKey = `${escape(process.env.TWITTER_CONSUMER_SECRET)}&${escape(process.env.TWITTER_ACCESS_TOKEN_SECRET)}`;
-
-  var signature = CryptoJS.enc.Base64.stringify(CryptoJS.HmacSHA1(base, signingKey));
-
-  var headerString = `OAuth oauth_consumer_key="${escape(key)}", oauth_callback="${escape('https://127.0.1.1/auth')}", oauth_nonce="${escape(nonce)}", oauth_signature="${escape(signature)}", oauth_signature_method="${method}", oauth_timestamp="${seconds}", oauth_token="${escape(access_token)}", oauth_version="${version}"`
-
-  console.log(headerString);
-
-  var options = {
-    url: 'https://api.twitter.com/oauth/request_token',
-    method: 'POST',
-    headers: {
-      'Authorization': headerString
-    }
-  };
-
-  request(options, function (error, response, body) {
-    console.log(body)
-  })
+  res.render('index', {title: "Instagram"})
 });
 
 router.get('/auth', function(req, res){
-  console.log(req.body);
+  var body = {
+    client_id: process.env.INSTAGRAM_ID,
+    client_secret: process.env.INSTAGRAM_SECRET,
+    grant_type: 'authorization_code',
+    redirect_uri: 'http://localhost:3000/auth',
+    code: req.query.code
+  }
+
+  var options = {
+    url: 'https://api.instagram.com/oauth/access_token',
+    method: 'POST',
+    body: querystring.stringify(body)
+  };
+
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body);
+      res.send(`Welcome, ${JSON.parse(body).user.username}!`);
+    }
+  })
 })
 
 module.exports = router;
